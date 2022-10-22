aad09a8545f8d96a282154247c92923c7525abbc
James Victor Alvarez <89657389+JamesVictorAlvarez@users.noreply.github.com>
2022-10-22 19:53:17 -0400

    Added Files

---
+++ Assignment2/Assign2/js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    $("#button1").click(function() {
+    $("#list1 option:selected").each(function() {
+        $(this).remove().appendTo("#list2");
+    });
+});
+
+$("#button2").click(function() {
+    $("#list2 option:selected").each(function() {
+        $(this).remove().appendTo("#list1");
+    });
+});
+
+    var reg = /^(?!.*[DFIOQU])[A-VXY][0-9][A-Z]●?[0-9][A-Z][0-9]$/;
+    $("#validateButton").click(function() {
+        if (reg.test($("#zip").val())) {
+            alert("Match");
+        }
+        else {
+            alert("Error");
+        }
+    })
+
+
+});
+
