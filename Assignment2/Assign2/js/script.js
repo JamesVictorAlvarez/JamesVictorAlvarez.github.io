8961d162604317c1b25132901ff6016adea643a0
James Victor Alvarez <89657389+JamesVictorAlvarez@users.noreply.github.com>
2022-10-22 20:22:16 -0400

    Fix buttons

--- Assignment2/Assign2/js/script.js
+++ Assignment2/Assign2/js/script.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-    $("#button1").click(function() {
+    $(".button1").click(function() {
     $("#list1 option:selected").each(function() {
         $(this).remove().appendTo("#list2");
     });
 });
 
-$("#button2").click(function() {
+$(".button2").click(function() {
     $("#list2 option:selected").each(function() {
         $(this).remove().appendTo("#list1");
     });
