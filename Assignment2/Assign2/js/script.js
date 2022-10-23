40f2cd9c915ffe8605fc9d4314b1cf44fd0011d9
James Victor Alvarez <89657389+JamesVictorAlvarez@users.noreply.github.com>
2022-10-23 00:03:27 -0400

    Improved alert with animation, Added footer

--- Assignment2/Assign2/js/script.js
+++ Assignment2/Assign2/js/script.js
@@ -1,26 +1,49 @@
-$(document).ready(function() {
-    $(".button1").click(function() {
-    $("#list1 option:selected").each(function() {
-        $(this).remove().appendTo("#list2");
+$(document).ready(function () {
+    //Revove string in list1 and put in list2
+    $(".button1").click(function () {
+        $("#list1 option:selected").each(function () {
+            $(this).remove().appendTo("#list2");
+        });
     });
-});
 
-$(".button2").click(function() {
-    $("#list2 option:selected").each(function() {
-        $(this).remove().appendTo("#list1");
+    //Revove string in list1 and put in list2
+    $(".button2").click(function () {
+        $("#list2 option:selected").each(function () {
+            $(this).remove().appendTo("#list1");
+        });
     });
-});
 
+    //Check if Zip Code is correct
     var reg = /^(?!.*[DFIOQU])[A-VXY][0-9][A-Z]●?[0-9][A-Z][0-9]$/;
-    $("#validateButton").click(function() {
+    $("#validateButton").click(function () {
         if (reg.test($("#zip").val())) {
-            alert("Match");
+            $(".match").removeClass("hide");
+            $(".match").addClass("show");
+            $(".match").addClass("showAlert");
+            setTimeout(function() {
+                $(".match").addClass("hide");
+                $(".match").removeClass("show");
+            }, 4000);
         }
         else {
-            alert("Error");
+            $(".alert").removeClass("hide");
+            $(".alert").addClass("show");
+            $(".alert").addClass("showAlert");
+            setTimeout(function() {
+                $(".alert").addClass("hide");
+                $(".alert").removeClass("show");
+            }, 4000);
         }
     })
 
+    $(".close-btn").click(function() {
+        $(".alert").addClass("hide");
+        $(".alert").removeClass("show");
+    })
 
+    $(".close-btn1").click(function() {
+        $(".match").addClass("hide");
+        $(".match").removeClass("show");
+    })
 });
 
