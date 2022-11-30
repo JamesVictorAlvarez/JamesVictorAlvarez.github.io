acd8ff35d808505cb3d30ac98e57c9b4aa1afd85
James Victor Alvarez <2182866@edu.vaniercollege.qc.ca>
2022-11-30 10:53:02 -0500

    Fixed daily img, started image gallery

--- Project/js/script.js
+++ Project/js/script.js
@@ -1,11 +1,15 @@
 $.ajax({
     url: "https://api.nasa.gov/planetary/apod?api_key=Txu7JGdMoSgOHZuZqXfX3VDlpUUoaA4I8i49a7nB",
     method: "GET",
-    success: function(response){
+    success: function(response) {
         console.log(response)
         document.getElementById("nasa-title").textContent = response.title
-        // document.getElementById("date").textContent = response.date
         document.getElementById("nasa-img").src = response.url
         document.getElementById("nasa-description").textContent = response.explanation
     }
-});  
+});
+
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+c.width = document.body.clientWidth;
+c.height = document.body.clientHeight;
