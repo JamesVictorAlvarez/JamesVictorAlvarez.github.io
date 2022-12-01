9e898c219f0e2c5ef148a10440e0ae324d77c475
James Victor Alvarez <2182866@edu.vaniercollege.qc.ca>
2022-12-01 10:54:59 -0500

    Finish About me

--- Project/js/marsScript.js
+++ Project/js/marsScript.js
@@ -1,7 +1,7 @@
 var images = $(".image1, .image2, .image3, .image4, .image5, .image6, .image7, .image8, .image9, .image10, .image11, .image12, .image13, .image14, .image15, .image16, .image17, .image18");
 
 $.ajax({
-    url: "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=2&api_key=Txu7JGdMoSgOHZuZqXfX3VDlpUUoaA4I8i49a7nB",
+    url: "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=6&api_key=Txu7JGdMoSgOHZuZqXfX3VDlpUUoaA4I8i49a7nB",
     method: "GET",
     success: function(response) {
         for (var i = 0; i < images.length; i++) {
